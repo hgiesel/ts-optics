4d31d430d2039cd4b59060da333a8ce67838a41f
Henrik Giesel <hengiesel@gmail.com>
2021-04-17 15:33:36 +0200

    Correctly implement function profunctor

--- src/profunctors/functions.ts
+++ src/profunctors/functions.ts
@@ -1,22 +1,21 @@
 import { Tuple, Either, isRight } from "../types";
-import { Profunctor, Strong, Choice } from "./profunctors";
+import { Strong, Choice } from "./profunctors";
 
-const dimap = <A, B, C, D>(
-  l: (a: A) => B,
-  r: (c: C) => D,
-  f: (b: B) => C
-): ((a: A) => D) => (a: A): D => r(f(l(a)));
+const dimap = <T, U, S, V>(l: (s: S) => T, r: (u: U) => V, f: (t: T) => U) => (
+  s: S
+): V => r(f(l(s)));
 
-const firstStrong = <A, B, C>(f: (a: A) => B) => (
-  tuple: Tuple<A, C>
-): Tuple<B, C> => [f(tuple[0]), tuple[1]];
+const firstStrong = <T, U, E>(f: (t: T) => U) => (
+  tuple: Tuple<T, E>
+): Tuple<U, E> => [f(tuple[0]), tuple[1]];
 
 const rightChoice = <A, B, C>(f: (a: A) => B) => (
   either: Either<C, A>
 ): Either<C, B> =>
   isRight(either) ? [true, f(either[1])] : [false, either[1]];
 
-export default new (class implements Profunctor, Strong, Choice {
+export default new (class<T, U>
+  implements Strong<T, U>, Choice<T, U> {
   dimap = dimap;
   firstStrong = firstStrong;
   rightChoice = rightChoice;
--- src/profunctors/profunctors.ts
+++ src/profunctors/profunctors.ts
@@ -1,17 +1,13 @@
 import { Tuple, Either } from "../types";
 
-export interface Profunctor {
-  dimap: <A, B, C, D>(
-    l: (a: A) => B,
-    r: (c: C) => D,
-    f: (b: B) => C
-  ) => (a: A) => unknown;
+export interface Profunctor<T, U> {
+  dimap: <S, V>(l: (s: S) => T, r: (u: U) => V, f: (t: T) => U) => (s: S) => V;
 }
 
-export interface Strong {
-  firstStrong: <A, B, C>(f: (a: A) => B) => (tuple: Tuple<A, C>) => unknown;
+export interface Strong<T, U> extends Profunctor<T, U> {
+  firstStrong: <E>(f: (t: T) => U) => (tuple: Tuple<T, E>) => Tuple<U, E>;
 }
 
-export interface Choice {
-  rightChoice: <A, B, C>(f: (a: A) => B) => (either: Either<C, A>) => unknown;
+export interface Choice<T, U> extends Profunctor<T, U>  {
+  rightChoice: <E>(f: (t: T) => U) => (either: Either<E, T>) => Either<E, U>;
 }
