c0a1c3e7a5e4ca00b4cbc196fd2e352e70314659
Henrik Giesel <hengiesel@gmail.com>
2021-04-17 14:45:03 +0200

    First compilation

---
+++ rollup.config.js
@@ -0,0 +1,56 @@
+import { nodeResolve } from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript'
+import babel from '@rollup/plugin-babel'
+import { terser } from "rollup-plugin-terser"
+
+const babelOptions = {
+    exclude: 'node_modules/**',
+    babelHelpers: 'bundled',
+}
+
+const terserOptions = {
+    output: {
+        comments: false,
+    },
+
+    compress: {
+        drop_console: false,
+        pure_funcs: [],
+    },
+}
+
+const tsconfigDist = {
+    declaration: true,
+    declarationDir: "dist/",
+}
+
+export default args => {
+    const destination = { file: 'dist/ts-optics.min.js' }
+    const format = 'esm'; // 'iife'
+
+    const plugins = [
+        nodeResolve(),
+        commonjs(),
+        typescript(args.configDist
+            ? tsconfigDist
+            : {}
+        ),
+        babel(babelOptions),
+    ]
+
+    if (!args.configDev) {
+        plugins.push(terser(terserOptions))
+    }
+
+    return {
+        input: 'index.ts',
+        output: {
+            name: 'ts-optics',
+            ...destination,
+            format,
+            sourcemap: true,
+        },
+        plugins,
+    }
+}
