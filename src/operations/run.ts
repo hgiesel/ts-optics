75234766856a8b498fb43f3a209bf1270edc4b92
Henrik Giesel <hengiesel@gmail.com>
2021-04-17 22:59:15 +0200

    Rewrite compose and run

--- src/operations/run.ts
+++ src/operations/run.ts
@@ -2,15 +2,7 @@ import type { Profunctor } from "../profunctors";
 import type { Optic } from "../constructors";
 
 export default <S, T, A, B, P extends Profunctor>(
-  optics: Optic<S, T, A, B, P>[],
+  optic: Optic<S, T, A, B, P>,
   dict: P,
   f: (a: A) => B
-): ((s: S) => T) => {
-  const evaluate = <X, Y>(
-    accu: (x: X) => Y,
-    optic: Optic<S, T, X, Y, P>
-  ): ((s: S) => T) => optic(dict, accu);
-
-  const reversed = optics.reverse();
-  return reversed.reduce(evaluate as any, f) as any;
-};
+): ((s: S) => T) => optic(dict, f);
